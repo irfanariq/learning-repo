eaf02057554c8fa198ed1f06bc9a16f2bff28024
irfan ariq <irfanariqzaki@gmail.com>
2022-01-12 11:17:02 +0900

    [new] leard code from leetcode. solve one question in top interview question - easy

---
+++ leetcode/top-interview-question_easy/array_1.py
@@ -0,0 +1,14 @@
+# Remove Duplicate from Sorted Array
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
+        idx = 1
+        for i in range(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                nums[idx] = nums[i]
+                idx += 1
+            # else:
+            #     idx += 1
+        return idx
